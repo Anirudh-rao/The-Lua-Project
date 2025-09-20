a91bf4469b6958b31b330c4408c4e254577fd020
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-09-20 13:49:24 +0530

    tables in lua

---
+++ 1.Fundemantals/TablesinLua.lua
@@ -0,0 +1,12 @@
+-- Tables are fundamental Data Structures in Lua
+
+function TableInLua()
+    local t = {} -- {} Define tables
+    t[1] = 10
+    t[2] = 20
+    t[3] = 30
+    print("Table is " ..t)
+    print("Length of Table is ")
+    print(#t)
+
+end
