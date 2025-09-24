fbe17f7a73b41c58815b34bbc1401ee3a69acfc2
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-09-24 15:03:24 +0530

    Updates

---
+++ 2.Functions/MultipleReturns.lua
@@ -0,0 +1,12 @@
+function maximum(s)
+    local max_index = 1  --intial case
+    local m = s[max_index]
+    for i = 2, #s do
+        if a[i]
+        then
+           m = s[i]
+           max_index = i
+        end
+    end
+    return m,max_index --return max and its index
+end
